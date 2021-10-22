206b1ad6c46ded11c3ff44555dca078f020c4000
Peluconus <peluconus@hotmail.com>
2021-10-22 10:13:37 +0200

    Subida inicial cliente config

---
+++ src/main/java/com/practica/configcliente/ConfigClienteApplication.java
@@ -0,0 +1,13 @@
+package com.practica.configcliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClienteApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/practica/configcliente/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.practica.configcliente.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@Value("${some.value}")
+	private String config;
+	
+	@GetMapping("/config")
+	public String myValue() {
+		return this.config;
+	}
+
+}
